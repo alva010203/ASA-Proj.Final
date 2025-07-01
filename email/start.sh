f8c608021518a6670dc5d10567ad89e728de9724
Jairo <jairoaraujobezerra@gmail.com>
2025-07-01 02:55:03 -0300

    fix: resolvendo problema de permissão

--- email/start.sh
+++ email/start.sh
@@ -2,9 +2,13 @@
 
 useradd -m -s /bin/false cara
 echo 'cara:cara123' | chpasswd
+chown -R cara:cara /home/cara
+
 
 useradd -m -s /bin/false coroa
 echo 'coroa:coroa123' | chpasswd
+chown -R coroa:coroa /home/coroa
+
 
 service dovecot start
 service syslog-ng start
